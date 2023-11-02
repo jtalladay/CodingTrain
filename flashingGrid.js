76c200592effd5fcba89a64d3df834bc85b4950c
jared <j.talladay@gmail.com>
2023-11-02 19:32:35 -0400

    update

---
+++ flashingGrid.js
@@ -0,0 +1,23 @@
+var inc = 0.01;
+var scl = 20;
+var cols, rows;
+function setup() {
+    createCanvas(400, 400);
+    cols = floor(width / scl);
+    rows = floor(height / scl);
+}
+
+function draw() {
+    var yoff = 0;
+    for (var y = 0; y < rows; y++){
+        var xoff = 0;
+        for (var x = 0; x < cols; x++){
+            var index = (x + y * width) * 4;
+            var r = noise(xoff, yoff) * 255;
+            xoff += inc;
+            fill(random(255));
+            rect(x * scl, y * scl, scl, scl);
+        }
+        yoff += inc;
+    }
+}
--- flowField.js
+++ flowField.js
@@ -5,20 +5,11 @@ var fr;
 
 var zoff = 0;
 
-var particles = [];
-var numParticles = 1000;
-var flowfield;
-
 function setup() {
     createCanvas(400, 400);
     cols = floor(width / scl);
     rows = floor(height / scl);
     fr = createP('');
-    flowfield = new Array(cols * rows);
-    for (var i = 0; i < numParticles; i++) {
-        particles[i] = new Particle();
-    }
-
 }
 
 function draw() {
@@ -27,14 +18,14 @@ function draw() {
     for (var y = 0; y < rows; y++){
         var xoff = 0;
         for (var x = 0; x < cols; x++){
-            var index = x + y * cols;
+            var index = (x + y * width) * 4;
             var angle = noise(xoff, yoff, zoff) * TWO_PI;
-            var v = p5.Vector.fromAngle(angle);
-            v.setMag(1);
-            flowfield[index] = v;
             xoff += inc;
-            stroke(0,50);
-            strokeWeight(1);
+            //fill(r);
+            // create vector here
+            var v = p5.Vector.fromAngle(angle);
+            //rect(x * scl, y * scl, scl, scl);
+            stroke(0);
             push();
             translate(x * scl, y * scl);
             rotate(v.heading());
@@ -45,12 +36,7 @@ function draw() {
         zoff += 0.0005;
 
     }
-    for (var i = 0; i < particles.length; i++) {
-        particles[i].follow(flowfield);
-        particles[i].update();
-        particles[i].show();
-        particles[i].edges();
-    }
-    // show framerate at bottom of canvas
+
+
     fr.html(floor(frameRate()));
 }
